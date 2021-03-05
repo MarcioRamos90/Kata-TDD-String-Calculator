57b5dfa40a1414da67d55e1da06b8ed7de76f082
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-03-05 08:41:28 -0300

    feat: ensure StringCalculator.add supports different delimiters when the string has this configuration: “//[delimiter]\\n[numbers...]”

--- src/string-calculator.js
+++ src/string-calculator.js
@@ -1,12 +1,29 @@
 class StringCalculator {
   constructor() {
-    this.re = /[, | \n]/
+    this.defaultDelimiter = ','
+    this.constructDelimiters()
+  }
+  
+  constructDelimiters() {
+    this.delimiterRegex = new RegExp(`[${this.defaultDelimiter} | \n]`)
+  }
+
+  verifyInput(numbers = '') {
+    if (/^\$\$/.test(numbers)) {
+      this.defaultDelimiter = '\;'
+
+      this.constructDelimiters()
+
+      return numbers.slice(3)
+    }
+    return numbers
   }
 
   add(numbers = "") {
+    numbers = this.verifyInput(numbers)
     if (!numbers) return 0
-    if (numbers.split(this.re).length === 1) return +numbers
-    let numbersConverted = numbers.split(this.re).map(numberString => +numberString)
+    if (numbers.split(this.delimiterRegex).length === 1) return +numbers
+    let numbersConverted = numbers.split(this.delimiterRegex).map(numberString => +numberString)
     return numbersConverted.reduce((prev, curr) => prev + curr, 0)
   }
 }
--- src/string-calculator.spec.js
+++ src/string-calculator.spec.js
@@ -2,13 +2,13 @@ const StringCalculator = require('./string-calculator').StringCalculator
 
 describe('String Calculator', () => {
   it('Should returns 0 if StringCalculator.add receive an empty string', () => {
-    const sut = new StringCalculator('')
+    const sut = new StringCalculator()
     const result = sut.add('')
     expect(result).toBe(0)
   })
 
   it('Should returns the number if StringCalculator.add receive "any number string"', () => {
-    const sut = new StringCalculator('')
+    const sut = new StringCalculator()
     expect(sut.add('3')).toBe(3)
     expect(sut.add('11')).toBe(11)
     expect(sut.add('111321414')).toBe(111321414)
@@ -17,7 +17,7 @@ describe('String Calculator', () => {
   })
 
   it('Should returns the sum if StringCalculator.add receive "number, number"', () => {
-    const sut = new StringCalculator('')
+    const sut = new StringCalculator()
     expect(sut.add('3, 2')).toBe(5)
     expect(sut.add('4, 23')).toBe(27)
     expect(sut.add('-4, 23')).toBe(19)
@@ -25,15 +25,22 @@ describe('String Calculator', () => {
   })
 
   it('Should StringCalculator.add returns the sum if are provided an unknown amount of numbers', () => {
-    const sut = new StringCalculator('')
+    const sut = new StringCalculator()
     expect(sut.add('3, 2 , 1 , 3 , 4')).toBe(13)
     expect(sut.add('3, 2 , 1 , 3 , 4, 1, 1, 100, 215')).toBe(330)
   })
 
   it('ensure StringCalculator.add handle new lines between numbers (instead of commas)', () => {
-    const sut = new StringCalculator('')
+    const sut = new StringCalculator()
     expect(sut.add('3\n2')).toBe(5)
     expect(sut.add('3\n2, 4')).toBe(9)
     expect(sut.add('3\n2, 4 \n 20')).toBe(29)
+  })
+
+  it('Should support different delimiters when the string has this configuration: “//[delimiter]\\n[numbers...]”', () => {
+    const sut = new StringCalculator()
+    expect(sut.add('$$;\n1;2')).toBe(3)
+    expect(sut.add('$$;\n1;2;4')).toBe(7)
+    expect(sut.add('$$;\n1;2;4; 10; 10')).toBe(27)
   })
 })
